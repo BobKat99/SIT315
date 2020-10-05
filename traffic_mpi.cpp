0544b2b528596e2002b26f7db439c98abc828077
BobKat99 <40940935+BobKat99@users.noreply.github.com>
2020-10-06 10:40:35 +1100

    Update traffic_mpi.cpp

--- traffic_mpi.cpp
+++ traffic_mpi.cpp
@@ -14,6 +14,104 @@
 using namespace std;
 using nano_s = chrono::nanoseconds;
 
+// the data in general
+struct trafficData {
+    int hours, minute, id, cars;
+};
+
+// the data after processing to create the sum
+struct resultSum {
+    int id=-1, sum;
+};
+
+// the below is for creating a type of queue corresponding witht the customized dataType
+// define default capacity of the queue
+#define SIZE 10
+// Class for queue
+class queue
+{
+	trafficData *arr;   	// array to store queue elements
+	int capacity;   // maximum capacity of the queue
+	int front;  	// front points to front element in the queue (if any)
+	int rear;   	// rear points to last element in the queue
+	int count;  	// current size of the queue
+
+public:
+	queue(int size = SIZE);	 // constructor
+	~queue();				   // destructor
+
+	void dequeue();
+	void enqueue(trafficData x);
+	trafficData peek();
+	int size();
+	bool isEmpty();
+	bool isFull();
+};
+// Constructor to initialize queue
+queue::queue(int size)
+{
+	arr = new trafficData[size];
+	capacity = size;
+	front = 0;
+	rear = -1;
+	count = 0;
+}
+// Destructor to free memory allocated to the queue
+queue::~queue()
+{
+	delete[] arr;
+}
+// Utility function to remove front element from the queue
+void queue::dequeue()
+{
+	// check for queue underflow
+	if (isEmpty())
+	{
+		cout << "UnderFlow\nProgram Terminated\n";
+		exit(EXIT_FAILURE);
+	}
+	front = (front + 1) % capacity;
+	count--;
+}
+// Utility function to add an item to the queue
+void queue::enqueue(trafficData item)
+{
+	// check for queue overflow
+	if (isFull())
+	{
+		cout << "OverFlow\nProgram Terminated\n";
+		exit(EXIT_FAILURE);
+	}
+	rear = (rear + 1) % capacity;
+	arr[rear] = item;
+	count++;
+}
+// Utility function to return front element in the queue
+trafficData queue::peek()
+{
+	if (isEmpty())
+	{
+		cout << "UnderFlow\nProgram Terminated\n";
+		exit(EXIT_FAILURE);
+	}
+	return arr[front];
+}
+// Utility function to return the size of the queue
+int queue::size()
+{
+	return count;
+}
+// Utility function to check if the queue is empty or not
+bool queue::isEmpty()
+{
+	return (size() == 0);
+}
+// Utility function to check if the queue is full or not
+bool queue::isFull()
+{
+	return (size() == capacity);
+}
+
 #define NUMBER_HOUR 4
 #define NUMBER_SIGN 6
 #define MINUTE_MEA 5
@@ -30,6 +128,8 @@ int **A;
 
 void init(int** &matrix, int rows, int cols, bool initialise);
 void print( int** matrix, int rows, int cols);
+void producer(string myText, queue &que);
+void consumer();
 void head(int num_processes);
 void node(int process_rank, int num_processes);
 
@@ -75,17 +175,21 @@ void head(int num_processes)
             MPI_Send(str.c_str(), str.length(), MPI_CHAR, i, 0, MPI_COMM_WORLD);
         }
     }
-
-    // ifstream MyReadFile2("data.csv");
-    // MyReadFile2.read(arrStr, sizeChar);
     
-    // int num_elements_to_scatter_or_gather = str.length() / num_processes;
+    queue que(100);
+    
+    MyReadFile.close();
+    stringstream str_strm(work_str);
+    for (int i = 0; i < num_data_local; i++) {
+        string record;
+        getline(str_strm, record);
+        producer(record, que);
+    }
+    
+    consumer(que, matrixResult);
 
     // MPI_Scatter(&arrStr[0], num_elements_to_scatter_or_gather ,  MPI_CHAR , &arrStr , 0, MPI_INT, 0 , MPI_COMM_WORLD);
-
-    // for (int i =0; i < num_elements_to_scatter_or_gather; i++) {
-    //     cout << arrStr[i];
-    // }
+    
     // MPI_Gather(MPI_IN_PLACE, num_elements_to_scatter_or_gather , MPI_INT, &C[0][0] , num_elements_to_scatter_or_gather , MPI_INT, 0 , MPI_COMM_WORLD);
 }
 void node(int process_rank, int num_processes)
@@ -137,4 +241,89 @@ void print( int** A, int rows, int cols) {
         printf("\n"); //at the end of the row, print a new line
     }
     printf("----------------------------\n");
+}
+
+bool qAccess(bool isProducer, trafficData data) {
+    if(isProducer) {
+        if (!que.isFull()) {
+            que.enqueue(data);
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        if (!que.isEmpty()) {
+            trafficData theData = que.peek();
+            if (matrixResult[theData.hours][theData.id].sum == 0) {
+                resultSum finalResult{theData.id, theData.cars};
+                matrixResult[theData.hours][theData.id] = finalResult;
+            } else {
+                matrixResult[theData.hours][theData.id].sum += theData.cars;
+            }
+            que.dequeue();
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+void producer(string myText, queue &que) {
+        //make a stringstream of the result which the rows inside the dataset
+//myText here is the line of data
+        stringstream str_strm(myText);
+        trafficData data;
+        string tmp;
+        int count = 0;
+        while (getline(str_strm, tmp, ',')) {
+            switch (count)
+            {
+                case 0:
+                    {
+                        stringstream s(tmp);
+                        string tmp2;
+                        int count2 = 0;
+                        while (getline(s, tmp2, ':')) {
+                            switch (count2)
+                            {
+                            case 0:
+                                data.hours = stoi(tmp2);
+                                break;
+                            case 1:
+                                data.minute = stoi(tmp2);
+                                break;
+                            default:
+                                cout << "error2" << endl;
+                                break;
+                            }
+                            count2++;
+                        }
+                    }
+                    break;
+                case 1:
+                    data.id = stoi(tmp);
+                    break;
+                case 2:
+                    data.cars = stoi(tmp);
+                    break;
+                default:
+                    cout << "error" << endl;
+                    break;
+            }   
+            count++;
+        }
+        bool check = false;
+        while(!check) {
+            check = qAccess(true, data);
+        }
+}
+
+
+void consumer(queue que, resultSum matrix[10][10]) {
+    while(!productionDone) {
+        while(!que.isEmpty()) {
+            qAccess(false, trafficData{0,0});
+        }
+    }
 }
